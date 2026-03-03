745bca2214f8a34a3bfd31fefd22102a270b168b
jarvis1704 <dasbiprangshu@gmail.com>
2026-03-03 11:37:44 +0530

    fix: dark and light mode toggle

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,7 @@
 /** @type {import('tailwindcss').Config} */
 
 const config = { 
+  darkMode: 'class',
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
